f9848bcb556a71f24de0eabdd886482d5409ae9a
wakhu shibero <wshibero@gmail.com>
2024-05-21 05:37:38 +0000

    added the jokes

--- jokes/views.py
+++ jokes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from pyjokes import get_joke
 
 # Create your views here.
 
 def index(request):
-  return render(request, 'index.html')
+  return render(request, 'index.html', {"joke": get_joke(language="en", category="all")})
